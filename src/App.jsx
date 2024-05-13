a43f1e4e29b090057bcf458499261d1e36b33e7a
Abhinav Abhishek <121249325+AbhinavAbhishek77@users.noreply.github.com>
2024-05-13 11:01:40 +0530

    Shopping Cart App Uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { Routes } from "react-router-dom";
-import Navbar from "./components/Navbar"
+
+import Navbar from "./components/Navbar";
 import { Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Cart from "./pages/Cart"
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return (<div>
-        <div style={{ backgroundColor: '#475569'}}>
-          <Navbar/>
-        </div>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/cart" element={<Cart/>} />
-        </Routes>
-  </div>)
+  return (
+    <div>
+      <div style={{ backgroundColor: "#475569" }}>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
