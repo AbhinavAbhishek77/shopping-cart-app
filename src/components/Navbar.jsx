3d954670cd756dcd96eee4b47cde50da24b692fd
Abhinav Abhishek <abhinavabhishek@Abhinavs-MacBook-Air.local>
2024-09-14 01:11:04 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,27 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-
+import logo2 from "../img/cartify-remove.png"
 const Navbar = () => {
   const { cart } = useSelector((state) => state);
 
   return (
-    <div style={{ backgroundColor: "#475569" }}>
+    <div style={{ backgroundColor: "#475569" }} >
       <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
         <NavLink to="/">
           <div className="ml-5">
             <img
-              src="https://codehelp-shopping-cart.netlify.app/logo.png"
-              className="h-14"
+              // src="https://codehelp-shopping-cart.netlify.app/logo.png"
+              src={logo2}
+              className="h-14 w-36"
               alt=""
             />
           </div>
         </NavLink>
 
         <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
-          <NavLink to="/">
-            <p>Home</p>
+          <NavLink to="/" className="hover:text-yellow-200">
+            <p className=" text-xl">Home</p>
           </NavLink>
 
           <NavLink to="/cart">
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -18,13 +18,13 @@ const Product = ({post}) => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-between 
+    <div className="flex flex-col items-center justify-between border-2 border-white
     hover:scale-110 transition duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl outline">
       <div>
-        <p className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">{post.title}</p>
+        <p className="text-white font-semibold text-lg text-left truncate w-40 mt-1">{post.title}</p>
       </div>
       <div>
-        <p className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0,10).join(" ") + "..."}</p>
+        <p className="w-40 text-gray-300  font-normal text-[10px] text-left">{post.description.split(" ").slice(0,10).join(" ") + "..."}</p>
       </div>
       <div className="h-[180px]">
         <img src={post.image} className="h-full w-full " alt="" />
@@ -38,17 +38,17 @@ const Product = ({post}) => {
         {
           cart.some((p) => p.id === post.id) ?
           (<button
-          className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
-          text-[12px] p-1 px-3 uppercase 
-          hover:bg-gray-700
+          className="text-white border-2 border-gray-700 rounded-full font-semibold 
+          text-[12px] p-1 px-3 uppercase bg-black
+          hover:bg-gray-900
           hover:text-white transition duration-300 ease-in"
           onClick={removeFromCart}>
             Remove Item
           </button>) :
           (<button
-          className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
-          text-[12px] p-1 px-3 uppercase 
-          hover:bg-gray-700
+          className="text-white border-2  rounded-full font-semibold 
+          text-[12px] p-1 px-3 uppercase bg-red border-white
+          hover:bg-gray-700 
           hover:text-white transition duration-300 ease-in"
           onClick={addToCart}>
             Add to Cart
